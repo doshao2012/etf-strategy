159363e8b66b1835db921ee7761d59b47f9704b5
user540924698 <2490867283404320-user540924698@noreply.coze.cn>
2026-04-24 12:51:52 +0800

    chore: 清理项目无用代码

--- server/src/etf-config/dto/update-etf-config.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEtfConfigDto } from './create-etf-config.dto';
-
-export class UpdateEtfConfigDto extends PartialType(CreateEtfConfigDto) {}
